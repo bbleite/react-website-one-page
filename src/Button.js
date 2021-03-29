465e35dd41aa70f9ff8017451d8ef767e8a6b8e4
Bernardo Bastos Leite <bbleite@hotmail.com>
2021-03-29 09:15:41 -0500

    navbar and menu button

---
+++ src/Button.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Button.css';
+
+const STYLES =['btn--primary', 'btn--outline']
+
+const SIZES = ['btn--medium', 'btn--large', 'btn--mobile', 'btn--wide']
+
+const COLOR = ['primary', 'blue', 'red', 'green']
+
+export const Button = ({
+    children, 
+    type, 
+    onClick, 
+    buttonStyle, 
+    buttonSize, 
+    buttonColor 
+}) => {
+    const checkButtonStyle = STYLES.includes(buttonStyle)
+
+    return (
+        <button className={`btn ${checkButtonStyle} ${checkButtonSize} ${checkButtonColor}`} onClick={onClick} type={type}>{children}</button>
+    )
+}
+
