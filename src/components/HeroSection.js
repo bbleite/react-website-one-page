4b627a2b3002a9f2fcf66105b2631a5ef24731f2
Bernardo Bastos Leite <bbleite@hotmail.com>
2021-04-02 23:28:06 -0500

    finished app 2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,39 +1,65 @@
-import React from 'react'
-import { Button } from './Button'
-import { Link } from 'react-router-dom'
-import './HeroSection.css'
+import React from 'react';
+import './HeroSection.css';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 
 function HeroSection({
-    lightBg, topline, lightText, lightTextDesc, headline, description,
-    buttonLable, img, alt, imgStart
+  lightBg,
+  topLine,
+  lightText,
+  lightTextDesc,
+  headline,
+  description,
+  buttonLabel,
+  img,
+  alt,
+  imgStart
 }) {
-    return (
-        <>
-            <div className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}>
-                <div className="onntainer">
-                    <divc className="row homw__hero-row"
-                        style={{ display: 'flex', flexDirection: imgStart === 'start' ? 'row-reverse' : "row" }}
-                    >
-                        <div className="col">
-                            <div className="home__hero-text-wrapper">
-                                <div className="top-line">{topline}</div>
-                                <h1 className={lightText ? 'heading' : 'heading dark'}>{headline}</h1>
-                                <p className={lightTextDesc ? 'home__hero-subtitle' : 'home__hero-subtitle dark'}>{description}</p>
-                                <Link to="/sign-up">
-                                    <Button buttonSize='btn-wide' buttonColor='blue'>{buttonLable}</Button>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className="home-img-wrapper">
-                                <img src={img} alt={alt} className="home__hero-img" />
-                            </div>
-                        </div>
-                    </divc>
-                </div>
+  return (
+    <>
+      <div
+        className={lightBg ? 'home__hero-section' : 'home__hero-section darkBg'}
+      >
+        <div className='container'>
+          <div
+            className='row home__hero-row'
+            style={{
+              display: 'flex',
+              flexDirection: imgStart === 'start' ? 'row-reverse' : 'row'
+            }}
+          >
+            <div className='col'>
+              <div className='home__hero-text-wrapper'>
+                <div className='top-line'>{topLine}</div>
+                <h1 className={lightText ? 'heading' : 'heading dark'}>
+                  {headline}
+                </h1>
+                <p
+                  className={
+                    lightTextDesc
+                      ? 'home__hero-subtitle'
+                      : 'home__hero-subtitle dark'
+                  }
+                >
+                  {description}
+                </p>
+                <Link to='/sign-up'>
+                  <Button buttonSize='btn--wide' buttonColor='blue'>
+                    {buttonLabel}
+                  </Button>
+                </Link>
+              </div>
             </div>
-        </>
-    )
+            <div className='col'>
+              <div className='home__hero-img-wrapper'>
+                <img src={img} alt={alt} className='home__hero-img' />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default HeroSection
+export default HeroSection;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { MdFingerprint } from "react-icons/md";
-import { FaBars, FaTimes } from "react-icons/fa";
-import { Button } from "./Button";
+import React, { useState, useEffect } from 'react';
+import { Button } from './Button';
+import { Link } from 'react-router-dom';
 import './Navbar.css';
-import { IconContext } from 'react-icons/lib'
+import { MdFingerprint } from 'react-icons/md';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IconContext } from 'react-icons/lib';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -23,54 +23,69 @@ function Navbar() {
 
   useEffect(() => {
     showButton();
+    window.addEventListener('resize', showButton);
+    return {
+      
+    }
   }, []);
 
-  window.addEventListener('resize', showButton);
 
   return (
     <>
       <IconContext.Provider value={{ color: '#fff' }}>
-        <div className="navbar">
-          <div className=".navbar-container container">
-            <Link to='/' className="navbar-logo" onClick={closeMobileMenu}>
-              <MdFingerprint className="navbar-icon" />
-            LAVISH
-          </Link>
-            <div className="menu-icon" onClick={handleClick}>
+        <nav className='navbar'>
+          <div className='navbar-container container'>
+            <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+              <MdFingerprint className='navbar-icon' />
+              LAVISH
+            </Link>
+            <div className='menu-icon' onClick={handleClick}>
               {click ? <FaTimes /> : <FaBars />}
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-              <li className="nav-item">
-                <Link to='/' className="nav-links" onClick={closeMobileMenu}>
+              <li className='nav-item'>
+                <Link to='/' className='nav-links' onClick={closeMobileMenu}>
                   Home
-                  </Link>
+                </Link>
               </li>
-              <li className="nav-item">
-                <Link to='/services' className="nav-links" onClick={closeMobileMenu}>
+              <li className='nav-item'>
+                <Link
+                  to='/services'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
                   Services
-                  </Link>
+                </Link>
               </li>
-              <li className="nav-item">
-                <Link to='/products' className="nav-links" onClick={closeMobileMenu}>
+              <li className='nav-item'>
+                <Link
+                  to='/products'
+                  className='nav-links'
+                  onClick={closeMobileMenu}
+                >
                   Products
-                  </Link>
+                </Link>
               </li>
-              <li className="nav-btn">
+              <li className='nav-btn'>
                 {button ? (
-                  <Link to='/sign-up' className="btn-link">
+                  <Link to='/sign-up' className='btn-link'>
                     <Button buttonStyle='btn--outline'>SIGN UP</Button>
                   </Link>
                 ) : (
-                  <Link to='/sign-up' className="btn-link" onClick={closeMobileMenu}>
-                    <Button buttonStyle='btn--outline' buttonSize='btn--mobile'>
+                  <Link to='/sign-up' className='btn-link'>
+                    <Button
+                      buttonStyle='btn--outline'
+                      buttonSize='btn--mobile'
+                      onClick={closeMobileMenu}
+                    >
                       SIGN UP
-                          </Button>
+                    </Button>
                   </Link>
                 )}
               </li>
             </ul>
           </div>
-        </div>
+        </nav>
       </IconContext.Provider>
     </>
   );
