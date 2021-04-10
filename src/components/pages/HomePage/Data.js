f336972701241c16b2a5c6cb1429e4794b0ce656
Bernardo Bastos Leite <bbleite@hotmail.com>
2021-04-09 23:05:37 -0500

    teste

--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -2,7 +2,7 @@ export const homeObjOne = {
     lightBg: false,
     lightText: true,
     lightTextDesc: true,
-    topLine: 'Exclusive Access',
+    topLine: 'Exclusive Access 1',
     headline: 'Unlimited Transactions with zero fees',
     description: 'Get access to our exclusive diamond card that allows you to send unlimited transactions without getting charged any fees',
     buttonLabel: 'Get Started',
